55e5396aab9c38f2b9b398c80350c9cc34c88d95
KarstenKu-Hub <karstenku@t-online.de>
2023-10-24 17:22:57 +0200

    ...

--- src/test.py
+++ src/test.py
@@ -1,1 +1,19 @@
-sql = "SELECT id FROM users WHERE username='" + user + "' AND password='" + pass + "'"
+from django.conf.urls import url
+from django.db import connection
+
+
+def show_user(request, username):
+    with connection.cursor() as cursor:
+        # BAD -- Using string formatting
+        cursor.execute("SELECT * FROM users WHERE username = '%s'" % username)
+        user = cursor.fetchone()
+
+        # GOOD -- Using parameters
+        cursor.execute("SELECT * FROM users WHERE username = %s", username)
+        user = cursor.fetchone()
+
+        # BAD -- Manually quoting placeholder (%s)
+        cursor.execute("SELECT * FROM users WHERE username = '%s'", username)
+        user = cursor.fetchone()
+
+urlpatterns = [url(r'^users/(?P<username>[^/]+)$', show_user)]
